e38a9ac055c943823ba51d792f24b808060339a0
deankatz <46748650+deankatz@users.noreply.github.com>
2019-03-31 03:55:48 +0300

    Add files via upload

---
+++ answers.js
@@ -0,0 +1,27 @@
+//WRITE YOUR ANSWERS HERE.
+
+//Push your results when finished
+
+const answers = {
+    1: "a",
+    2: "c",
+    3: "d",
+    4: "d",
+    5: "d",
+    6: "d",
+    7: "d",
+    8: "c",
+    9: "b",
+    10: "c",
+    11: "a",
+    12: "a",
+    13: "b",
+    14: "d",
+    15: "d",
+    16: "a",
+    17: "b",
+    18: "c",
+    19: "d",
+    20: "c",
+    21: "d",
+}
